f1c26594b2118bb1c85e826fa656eff8bd646d7d
NewellWalker <Newell.Walker.1@gmail.com>
2024-04-17 15:27:38 -0600

    Cardio selector changed to radio buttons

--- src/Selectors/ExerciseSelector/CardioSelector/CardioSelector.jsx
+++ src/Selectors/ExerciseSelector/CardioSelector/CardioSelector.jsx
@@ -5,23 +5,26 @@ export default function CardioSelector(props) {
 
     return(
         <div className='cardioSection'>
-            <label>
                 <strong>Do you want to do cardio?</strong>
                 <br />
                 <input 
-                    type="checkbox"
-                    label="cardioYes"
-                    onClick={props.handleCardio}
-                    disabled={props.primaryMuscleState === '' ? 'disabled' : ''}
-                />
-                    Yes
-                <input 
-                    type="checkbox"
-                    label="cardioNo"
-                    disabled={props.primaryMuscleState === '' ? 'disabled' : ''}
-                />
-                    No 
-            </label>          
+                type="radio"
+                name="cardio"
+                value="yes"
+                onChange={props.handleCardio}
+                disabled={props.primaryMuscleState === '' ? true : false}
+                checked={props.cardioState === true}
+            />
+            <label>Yes</label>
+            <input 
+                type="radio"
+                name="cardio"
+                value="no"
+                onChange={props.handleCardio}
+                disabled={props.primaryMuscleState === '' ? true : false}
+                checked={props.cardioState === false}
+            />
+            <label>No</label>
             <br />
             <br />
             {props.cardioState === true ? 
